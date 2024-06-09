4229ac0898efa38b924f67bc90de698dc969c91c
mairalala <mairaylala2000@gmail.com>
2024-06-08 20:12:06 -0500

    Backend

--- backend/server.js
+++ backend/server.js
@@ -1,67 +1,46 @@
-const bcrypt = require('bcrypt');
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
 
-const {tableCreation} = require('./schema/createTables');
-const tableNames = ["user", "restaurant", "review", "booking", "schedule"];
-
 app.use(cors());
 app.use(express.json());
 
-try {
-  app.use(cors());
+const db = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "curd",
+});
 
-  const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "curd",
-  });
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  if (connection) connection.release();
+  console.log("Connected to MySQL...");
+});
 
-  app.post("/login", (req, res) => {
-    const sql = "select * from user where usercol = ? AND  password = ?";
+app.post("/login", (req, res) => {
+  const sql = "SELECT * FROM user WHERE usercol = ? AND password = ?";
 
-    db.query(sql, [req.body.usercol, req.body.password], (error, data) => {
-      if (error) {
-        return res.json(error);
-      }
+  db.query(sql, [req.body.usercol, req.body.password], (error, data) => {
+    if (error) {
+      console.error("Error executing query:", error);
+      return res.status(500).json("Internal Server Error");
+    } else {
       if (data.length > 0) {
         return res.json("Usuario reconocido");
       } else {
         return res.json("Usuario no reconocido");
       }
-    });
-  });
-
-  app.post("/record", async (req, res) => {
-    const { username, email, password, confirmPassword } = req.body;
-  
-    if (password !== confirmPassword) {
-      return res.json({ error: "Passwords do not match" });
     }
-  
-    const hashedPassword = await bcrypt.hash(password, 10);
-  
-    const sql = "INSERT INTO record (username, email, password) VALUES (?, ?, ?)";
-  
-    db.query(sql, [username, email, hashedPassword], (error, result) => {
-      if (error) {
-        return res.json(error);
-      }
-      if (result.length > 0) {
-        return res.json("User registered successfully");
-      } else {
-        return res.json("User registered error");
-      }
-    });
   });
-} catch (error) {
-  console.log(error);
-}
+});
 
 app.listen(8081, () => {
-  console.log("Conectado MYSQL...");
-  console.log("Conectado DBA...");
+  console.log("Server running on port 8081...");
+  console.log("holi");
 });
